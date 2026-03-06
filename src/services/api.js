6fdbab8707d9749600e8cea761b16fe3a6ae1623
SalmanIbrahimShaikh <salmanshaikh1693@gmail.com>
2026-03-07 02:55:33 +0530

    fix api headers

--- src/services/api.js
+++ src/services/api.js
@@ -2,13 +2,20 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://task-manager-s0b7.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 });
 
+// Add token automatically in every request
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
+
   return req;
 });
 
